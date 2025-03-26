00620983b6be1a4256468b1e3208bbf337b3eea0
Dan Ivan E. Labin <danivanlabin@gmail.com>
2025-03-26 21:13:19 +0800

    commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,4 +14,7 @@ class User extends Model
 
     public $timestamps = false;
     protected $primaryKey = 'userid';
+    protected $hidden = [
+        'password',
+    ];
 }
--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,4 @@
+
 <?php
 
 /** @var \Laravel\Lumen\Routing\Router $router */
